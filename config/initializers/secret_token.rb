74f198074c5bc2c0d064aae52d825c3c1daaf444
vagrant <vagrant@rails-dev-box>
2014-10-18 16:30:37 +0000

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fakerisimo::Application.config.secret_key_base = '3008c10d3d2e987f32e9c19f537f1eb98eacb07acea32a526a3374c5cea13ff32431d082563952b0688f34d64e99640d3fc8f8327c616ad35aa59f2a6fbe66e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fakerisimo::Application.config.session_store :cookie_store, key: '_fakerisimo_session'
