535d7d8bcb04b40956943a4e5f23cd77358f6e62
vagrant <vagrant@rails-dev-box>
2014-10-19 02:10:39 +0000

    credit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018222700) do
+ActiveRecord::Schema.define(version: 20141019020945) do
+
+  create_table "credits", force: true do |t|
+    t.integer  "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
